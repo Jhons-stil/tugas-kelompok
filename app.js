3cf671310f6bcd81139167b1b9a8d9142d0539e9
Marisa <rajamangi@gmail.com>
2025-12-25 17:13:03 +0700

    update opsi 1

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 const readline = require("readline");
-const daftar = require("./data.js")
+const daftar = require("./data.js");
 const real = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 function tanya(pertanyaan) {
-    return new Promise((resolve) => {
-        real.question(pertanyaan, resolve);
-    });
-};
-
-
-function tampilkan () {
-console.log("\n--= ( WARUNG BABA CHAN ) =--\n");
-console.log("Daftar Menu:");
-
-daftar.menu.forEach((item) => {
-    console.log(`- (${item.kode}) ${item.nama} - ${item.harga}`);
-    
-})
-console.log("\n Opsi");
-console.log("1. Pesan");
-console.log("2. Daftar Pesanan Saya");
-console.log("3. Keluar");
-pilihOpsi();
-};
-
-async function pilihOpsi() {
-    const pilih = await tanya("Pilih (1/2/3): ");
+  return new Promise((resolve) => {
+    real.question(pertanyaan, resolve);
+  });
 }
-tampilkan()
+
+function tampilkan() {
+  console.log("\n--= ( WARUNG BABA CHAN ) =--\n");
+  console.log("Daftar Menu:");
+
+  daftar.menu.forEach((item) => {
+    console.log(`- (${item.kode}) ${item.nama} - ${item.harga}`);
+  });
+
+  pilihOpsi();
+}
+
+async function inputPesan() {
+  const kode = await tanya("Masukkan kode :");
+  const pesan = daftar.menu.find((data) => data.kode === kode);
+
+  daftar.pesanan.push(pesan);
+  console.log("Pesanan berhasil ditambahkan");
+
+  pilihOpsi()
+}
+async function pilihOpsi() {
+  console.log(`\n Opsi
+  1. Pesan
+  2. Daftar Pesanan Saya
+  3. Keluar`);
+
+  const pilih = parseInt(await tanya("Pilih (1/2/3) :"));
+  if (pilih === 3) {
+    console.log("Terima kasih telah berkunjung ke warung kami");
+    real.close();
+    return;
+  }
+
+  if (pilih === 1) {
+    inputPesan();
+    return;
+  }
+
+  if (pilih === 2) {
+    
+  }
+}
+tampilkan();
